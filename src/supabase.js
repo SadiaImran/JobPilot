4794dd0c9b88b6df193170526d2e09bc4a9145aa
SadiaImran <sadiaimran837@gmail.com>
2025-07-29 20:41:42 +0500

    updated checks

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 // src/supabase.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ueimpskrrqnaihwedvqh.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlaW1wc2tycnFuYWlod2VkdnFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEwMTQwMTUsImV4cCI6MjA2NjU5MDAxNX0.1LwzwmN2TzTv0kfVbAS1aJ25EmXjQoolUcqw2SXoDMc'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL 
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANNON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
