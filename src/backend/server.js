0084e7f4673eb5f1d5952254d45ae557efcee42a
SadiaImran <sadiaimran837@gmail.com>
2025-07-30 15:53:50 +0500

    cors is not coring

--- src/backend/server.js
+++ src/backend/server.js
@@ -18,7 +18,6 @@ const app = express();
 
 const allowedOrigins = [
   'https://job-pilot-phi.vercel.app',
-  'http://localhost:3000' 
 ];
 
 app.use(cors({
@@ -31,6 +30,13 @@ app.use(cors({
   },
   credentials: true,
 }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://job-pilot-phi.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(json({ limit: '5mb' }));
 
